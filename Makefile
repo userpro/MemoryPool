0e63194b9676561fc97e090d2410ac6e6d1378e7
zdz <437773935@qq.com>
2018-12-26 11:49:46 +0800

    add some info

--- Makefile
+++ Makefile
@@ -8,11 +8,11 @@ EXAMPLE_OUTPUT = example
 
 main:
 	$(CPP) $(MAIN_SOURCES) $(SOURCES) -o $(MAIN_OUTPUT).out
-	@echo "main ok!\n"
+	@echo "complie main ok!\n"
 
 example:
 	$(CC) $(EXAMPLE_SOURCES) $(SOURCES) -o $(EXAMPLE_OUTPUT).out
-	@echo "example ok!\n"
+	@echo "compile example ok!\n"
 
 run_main:
 	./$(MAIN_OUTPUT).out
--- main.cpp
+++ main.cpp
@@ -8,8 +8,8 @@
 // #define HARD_MODE // 该模式更接近随机分配释放内存的情景
 
 /* -------- 测试数据参数 -------- */
-#define MEM_SIZE (2*GB) // 内存池管理的每个内存块大小
-#define DATA_N 300000 // 数据条数
+#define MEM_SIZE (1*GB) // 内存池管理的每个内存块大小
+#define DATA_N 30000 // 数据条数
 #define DATA_MAX_SIZE (16*KB) // 每条数据最大尺寸
 #define MAX_N 3 // 总测试次数
 /* -------- 测试数据参数 -------- */
@@ -24,12 +24,6 @@
     #define GB (unsigned long long)(1<<30)
     #define My_Malloc(x) malloc(x)
     #define My_Free(x)   free(x)
-#endif
-
-#ifdef _z_memorypool_h_
-    mem_size_t total_size = 0, cur_size = 0;
-#else
-    unsigned long long total_size = 0, cur_size = 0, cnt = 0;
 #endif
 
 #define SHOW(x) do { \
@@ -48,49 +42,40 @@
     printf("\n"); \
 }while (0)
 
+#ifdef _z_memorypool_h_ // 全局变量记录内存池使用信息
+    mem_size_t total_size = 0, cur_size = 0;
+#else
+    unsigned long long total_size = 0, cur_size = 0, cnt = 0;
+#endif
+
 struct Node
 {
     int size;
     char *data;
-};
-
-Node mem[DATA_N];
-void _init()
-{
-    srand((unsigned)time(NULL));
-}
+    bool operator <(const Node& n) const { return size < n.size; }
+} mem[DATA_N];
 
 unsigned int random_uint(unsigned int maxn)
 {
     unsigned int ret = abs(rand()) % maxn;
-    return ret > 0 ? ret : 10;
-}
-
-bool _cmp(const Node &n1,const Node &n2)
-{
-    return n1.size<n2.size;
+    return ret > 0 ? ret : 32;
 }
 
 int main()
 {
-    _init();
-    // -------- clock start ---------
+    srand((unsigned)time(NULL));
     clock_t start, finish;
     double total_time;
     start = clock();
-    // -------- clock start ----------
 
-    // -------- pre --------
-#ifndef _z_memorypool_h_
+#ifndef _z_memorypool_h_ // 区分系统malloc和内存池实现
     printf("System malloc:\n");
 #else
     printf("Memory Pool:\n");
     mem_size_t mlist_cnt = 0, free_cnt = 0, alloc_cnt = 0;
     MemoryPool *mp = MemoryPool_Init(MEM_SIZE, 1);
 #endif
-    // -------- pre --------
 
-    // -------- main --------
     for (int i = 0; i < 3; ++i)
     {
         printf("-------------------------\n");
@@ -105,9 +90,10 @@ int main()
             total_size += cur_size;
             mem[j].data = (char *)My_Malloc(cur_size);
             mem[j].size = cur_size;
+            *(int *)mem[j].data = 123;
         }
         // 排序进一步打乱内存释放顺序 模拟实际中随机释放内存
-        std::sort(mem, mem + DATA_N, _cmp);
+        std::sort(mem, mem + DATA_N);
 
 #ifdef HARD_MODE
         // 释放前一半管理的内存
@@ -120,19 +106,23 @@ int main()
             total_size += cur_size;
             mem[j].data = (char *)My_Malloc(cur_size);
             mem[j].size = cur_size;
+            *(int *)mem[j].data = 456;
         }
-        std::sort(mem, mem + DATA_N, _cmp);
+        std::sort(mem, mem + DATA_N);
 #endif
 
 #if (defined _z_memorypool_h_) && (defined ENABLE_SHOW)
         SHOW("Free Before: ");
 #endif
 
-        for (int j = 0; j < DATA_N; ++j)
+        for (int j = 0; j < DATA_N; ++j) {
+            // printf("%d ", *(int *)mem[j].data);
             My_Free(mem[j].data);
+        }
+        printf("\n");
         // MemoryPool_Clear(mp);
 
-#ifdef _z_memorypool_h_
+#ifdef _z_memorypool_h_ // 统计信息
         #ifdef ENABLE_SHOW 
             SHOW("Free After: ");
         #endif
@@ -140,13 +130,10 @@ int main()
 #endif
         printf("total_size: %.4lf MB\n", (double)total_size/1024/1024);
     }
-    // -------- main --------
 
-    // -------- clock end ----------
     finish = clock();
     total_time = (double)(finish - start) / CLOCKS_PER_SEC;
     printf("\nTotal time: %f seconds.\n", total_time);
-    // -------- clock end ----------
 
     return 0;
 }
